7b90551ff3e81855e98e962007c89aca0711259a
TheValentinov <115546191+TheValentinov@users.noreply.github.com>
2022-10-20 18:11:13 +0300

    Add files via upload

---
+++ css/cool.cs
@@ -0,0 +1,49 @@
+.menu::after,
+.menu::before {
+  content: "";
+  display: block;
+  height: 1px;
+  background-color: #dbdbdb;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 190px;
+}
+
+.menu::after {
+  top: 230px;
+}
+
+.header__top-up-left {
+      position: absolute;
+    top: 50px;
+    left: 60px;
+  margin-bottom: -8px;
+}
+
+.header__top-wrapper {
+    margin-top: 65px;
+    display: flex;
+    align-items: flex-end;
+}
+
+                        <div class="product-card_bottom">
+                          <div class="product-card__left-">
+                           <div class="product-card__left-left">
+                             <p class="card__last-price">4190</p>
+                             <img class="card__last-price-img" src="img/rubl-small.svg" alt="">
+                           </div>
+                            <div class="product-card__left-bottom">
+                              <span class="product-card__price"><span class="product-card__currency"></span>3 190</span>
+                              <img src="img/rubl.svg" alt="" class="card__price">
+
+                            </div>
+                          </div>
+                          <div class="product-card__right">
+                            <span class="product-card__like"><span class="product-card__currency"></span>В наличии</span>
+                            <button class="card__like" id="card_like"><img class="korzina_green" src="img/korzina_green.svg"
+                                alt=""><img class="korzina_blue" src="img/korzina_blue.svg" alt=""></button>
+
+                          </div>
+
+                        </div>
